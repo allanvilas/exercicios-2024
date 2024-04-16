6fe0681aefe11b201a5c7cd721a21d632b5a2530
allanvilas <allanvilasdeveloper@gmail.com>
2024-04-16 01:27:04 -0300

    Feat. Add method to handle and export the screapped data.

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
 
 require_once 'Scrapper.php';
 /**
  * Runner for the Webscrapping exercice.
  */
 class Main {
-
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
@@ -16,8 +17,64 @@ class Main {
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
     $data = (new Scrapper())->scrap($dom);
+    
+    self::WriteXLSX($data);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+  }
+
+  private static function WriteXLSX($data):void {
+
+    $filePath = __DIR__ . '\\Export\\Export.xlsx';
+
+    $writer = WriterEntityFactory::createXLSXWriter();
+    
+    $writer->openToFile($filePath);
+    
+    // Pre generate the readers
+    $titleCells = [
+      WriterEntityFactory::createCell('ID'),
+      WriterEntityFactory::createCell('Title'),
+      WriterEntityFactory::createCell('Type'),
+      WriterEntityFactory::createCell('Author 1'),
+      WriterEntityFactory::createCell('Author 1 Institution'),
+      WriterEntityFactory::createCell('Author 2'),
+      WriterEntityFactory::createCell('Author 2 Institution'),
+      WriterEntityFactory::createCell('Author 3'),
+      WriterEntityFactory::createCell('Author 3 Institution'),
+      WriterEntityFactory::createCell('Author 4'),
+      WriterEntityFactory::createCell('Author 4 Institution'),
+      WriterEntityFactory::createCell('Author 5'),
+      WriterEntityFactory::createCell('Author 5 Institution'),
+      WriterEntityFactory::createCell('Author 6'),
+      WriterEntityFactory::createCell('Author 6 Institution'),
+      WriterEntityFactory::createCell('Author 7'),
+      WriterEntityFactory::createCell('Author 7 Institution'),
+      WriterEntityFactory::createCell('Author 8'),
+      WriterEntityFactory::createCell('Author 8 Institution'),
+      WriterEntityFactory::createCell('Author 9'),
+      WriterEntityFactory::createCell('Author 9 Institution')
+    ];
+
+    $Header = WriterEntityFactory::createRow($titleCells);
+    $writer->addRow($Header);
+
+    // Add the scrapped data to the xlsx
+    foreach($data as $paper) {
+      $PaperRow = [
+        WriterEntityFactory::createCell($paper->id),  
+        WriterEntityFactory::createCell($paper->title),  
+        WriterEntityFactory::createCell($paper->type),  
+      ];
+      foreach($paper->authors as $key => $person) {
+        array_push($PaperRow, WriterEntityFactory::createCell(str_replace(';','',$person->name)));
+        array_push($PaperRow, WriterEntityFactory::createCell($person->institution));
+      }
+
+      $paperData = WriterEntityFactory::createRow($PaperRow);
+      $writer->addRow($paperData);
+    }
+    
+    $writer->close();
   }
 }
+
