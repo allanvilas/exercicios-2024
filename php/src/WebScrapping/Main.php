d6087aa41a88ffcd415b975b32e19c3f83aa8444
allanvilas <allanvilasdeveloper@gmail.com>
2024-04-17 20:43:14 -0300

    Fix: process .php files with phpcbf.

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,80 +1,86 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
+
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
-use Box\Spout\Common\Entity\Row;
 
 require_once 'Scrapper.php';
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main {
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+class Main
+{
+    /**
+     * Main runner, instantiates a Scrapper and runs.
+     */
+    public static function run(): void
+    {
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+        $data = (new Scrapper())->scrap($dom);
     
-    self::writeXlsx($data);
+        self::writeXlsx($data);
 
-  }
-
-  private static function writeXlsx($data):void {
-
-    $filePath = __DIR__ . '\\Export\\Export.xlsx';
-
-    $writer = WriterEntityFactory::createXLSXWriter();
-    
-    $writer->openToFile($filePath);
-    
-    // Pre generate the readers
-    $titleCells = [
-      WriterEntityFactory::createCell('ID'),
-      WriterEntityFactory::createCell('Title'),
-      WriterEntityFactory::createCell('Type'),
-      WriterEntityFactory::createCell('Author 1'),
-      WriterEntityFactory::createCell('Author 1 Institution'),
-      WriterEntityFactory::createCell('Author 2'),
-      WriterEntityFactory::createCell('Author 2 Institution'),
-      WriterEntityFactory::createCell('Author 3'),
-      WriterEntityFactory::createCell('Author 3 Institution'),
-      WriterEntityFactory::createCell('Author 4'),
-      WriterEntityFactory::createCell('Author 4 Institution'),
-      WriterEntityFactory::createCell('Author 5'),
-      WriterEntityFactory::createCell('Author 5 Institution'),
-      WriterEntityFactory::createCell('Author 6'),
-      WriterEntityFactory::createCell('Author 6 Institution'),
-      WriterEntityFactory::createCell('Author 7'),
-      WriterEntityFactory::createCell('Author 7 Institution'),
-      WriterEntityFactory::createCell('Author 8'),
-      WriterEntityFactory::createCell('Author 8 Institution'),
-      WriterEntityFactory::createCell('Author 9'),
-      WriterEntityFactory::createCell('Author 9 Institution')
-    ];
-
-    $header = WriterEntityFactory::createRow($titleCells);
-    $writer->addRow($header);
-
-    // Add the scrapped data to the xlsx
-    foreach($data as $paper) {
-      $paperRow = [
-        WriterEntityFactory::createCell($paper->id),  
-        WriterEntityFactory::createCell($paper->title),  
-        WriterEntityFactory::createCell($paper->type),  
-      ];
-      foreach($paper->authors as $key => $person) {
-        array_push($paperRow, WriterEntityFactory::createCell(str_replace(';','',$person->name)));
-        array_push($paperRow, WriterEntityFactory::createCell($person->institution));
-      }
-
-      $paperData = WriterEntityFactory::createRow($paperRow);
-      $writer->addRow($paperData);
     }
+
+    private static function writeXlsx($data):void
+    {
+        /**
+        * Xlsx exporter, build the xlsx based on scrapper data and export it.
+        */
+        $filePath = __DIR__ . '\\Export\\Export.xlsx';
+
+        $writer = WriterEntityFactory::createXLSXWriter();
     
-    $writer->close();
-  }
+        $writer->openToFile($filePath);
+    
+        // Pre generate the readers.
+        $titleCells = [
+        WriterEntityFactory::createCell('ID'),
+        WriterEntityFactory::createCell('Title'),
+        WriterEntityFactory::createCell('Type'),
+        WriterEntityFactory::createCell('Author 1'),
+        WriterEntityFactory::createCell('Author 1 Institution'),
+        WriterEntityFactory::createCell('Author 2'),
+        WriterEntityFactory::createCell('Author 2 Institution'),
+        WriterEntityFactory::createCell('Author 3'),
+        WriterEntityFactory::createCell('Author 3 Institution'),
+        WriterEntityFactory::createCell('Author 4'),
+        WriterEntityFactory::createCell('Author 4 Institution'),
+        WriterEntityFactory::createCell('Author 5'),
+        WriterEntityFactory::createCell('Author 5 Institution'),
+        WriterEntityFactory::createCell('Author 6'),
+        WriterEntityFactory::createCell('Author 6 Institution'),
+        WriterEntityFactory::createCell('Author 7'),
+        WriterEntityFactory::createCell('Author 7 Institution'),
+        WriterEntityFactory::createCell('Author 8'),
+        WriterEntityFactory::createCell('Author 8 Institution'),
+        WriterEntityFactory::createCell('Author 9'),
+        WriterEntityFactory::createCell('Author 9 Institution')
+        ];
+
+        $header = WriterEntityFactory::createRow($titleCells);
+        $writer->addRow($header);
+
+        // Add the scrapped data to the xlsx.
+        foreach($data as $paper) {
+            $paperRow = [
+            WriterEntityFactory::createCell($paper->id),  
+            WriterEntityFactory::createCell($paper->title),  
+            WriterEntityFactory::createCell($paper->type),  
+            ];
+            foreach($paper->authors as $key => $person) {
+                array_push($paperRow, WriterEntityFactory::createCell(str_replace(';', '', $person->name)));
+                array_push($paperRow, WriterEntityFactory::createCell($person->institution));
+            }
+
+            $paperData = WriterEntityFactory::createRow($paperRow);
+            $writer->addRow($paperData);
+        }
+    
+        $writer->close();
+    }
+
 }
 
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -11,85 +11,91 @@ require_once 'Entity/Person.php';
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    $domNodeElem = $dom->getElementsByTagName('a');
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array
+    {
+        $domNodeelem = $dom->getelementsByTagName('a');
     
-    $papers = [];
+        $papers = [];
 
-    foreach($domNodeElem as $Elem){
-      $classAtt = $Elem->getAttribute('class');
-      $elemChildNodes = $Elem->childNodes->length;
-      // Filter only target <a> cards
-      if( $elemChildNodes > 0 && strpos($classAtt, 'paper-card') !== false ) {
-        $id = $this->extractId($Elem);
-        $title = $this->extractTitle($Elem);
-        $type = $this->extractType($Elem);
-        $persons = $this->ExtractAuthors($Elem);
+        foreach($domNodeelem as $elem){
+            $classAtt = $elem->getAttribute('class');
+            $elemChildNodes = $elem->childNodes->length;
+            // Filter only target <a> cards
+            if($elemChildNodes > 0 && strpos($classAtt, 'paper-card') !== false ) {
+                $id = $this->extractId($elem);
+                $title = $this->extractTitle($elem);
+                $type = $this->extractType($elem);
+                $persons = $this->extractAuthors($elem);
         
-        $localPaper = new Paper($id,$title,$type,$persons);
-        array_push($papers,$localPaper);
-      }
-    }
-    return $papers;
-  }
-
-  private function extractTitle($domElem) : string{
-    try{
-      $title = $domElem->firstChild->nodeValue;
-      return $title;
-    } catch(Exception $e) { 
-      return 'Title not founded!';
-    } 
-  }
-
-  private function extractType($domElem) : string{
-    try{
-      $type = $domElem->childNodes->item(2)->firstChild->nodeValue;
-      return $type;
-    } catch(Exception $e) { 
-      return 'Type not founded!';
-    } 
-  }
-
-  private function extractId($domElem) : string{
-    try{
-      $link = $domElem->getAttribute('href');
-      $id = basename($link);
-      return $id;
-    } catch(Exception $e) { 
-      return 'Id not founded!';
-    } 
-  }
-
-  private function ExtractAuthors($domElem) : array{
-    try{
-      $authors = $domElem->childNodes->item(1)->childNodes;
-
-      $persons = [];
-
-      foreach($authors as $author){
-        if($author->hasAttributes()){
-
-          // Pushs persons to a staging array before return
-          $per = new Person(
-            $author->nodeValue, 
-            $author->getAttribute('title')
-          );
-
-          array_push($persons,$per);            
+                $localPaper = new Paper($id, $title, $type, $persons);
+                array_push($papers, $localPaper);
+            }
         }
-      }
-      return $persons;      
-    } catch(Exception $e) { 
-      return [
-        new Person('Person name not found','Person institution not found')
-      ];
-    } 
-  }
+        return $papers;
+    }
+
+    private function extractTitle($domelem) : string
+    {
+        try{
+            $title = $domelem->firstChild->nodeValue;
+            return $title;
+        } catch(Exception $e) { 
+            return 'Title not founded!';
+        } 
+    }
+
+    private function extractType($domelem) : string
+    {
+        try{
+            $type = $domelem->childNodes->item(2)->firstChild->nodeValue;
+            return $type;
+        } catch(Exception $e) { 
+            return 'Type not founded!';
+        } 
+    }
+
+    private function extractId($domelem) : string
+    {
+        try{
+            $link = $domelem->getAttribute('href');
+            $id = basename($link);
+            return $id;
+        } catch(Exception $e) { 
+            return 'Id not founded!';
+        } 
+    }
+
+    private function extractAuthors($domelem) : array
+    {
+        try{
+            $authors = $domelem->childNodes->item(1)->childNodes;
+
+            $persons = [];
+
+            foreach($authors as $author){
+                if($author->hasAttributes()) {
+
+                    // Pushs persons to a staging array before return
+                    $per = new Person(
+                        $author->nodeValue, 
+                        $author->getAttribute('title')
+                    );
+
+                    array_push($persons, $per);            
+                }
+            }
+            return $persons;      
+        } catch(Exception $e) { 
+            return [
+            new Person('Person name not found', 'Person institution not found')
+            ];
+        } 
+    }
 
 }
